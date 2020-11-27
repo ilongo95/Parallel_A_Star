b143035335d15d93911a73784f68ec50fcb17571
ilongo95 <34043374+ilongo95@users.noreply.github.com>
2020-11-27 10:11:06 -0500

    Added Distance Method

--- Graph.h
+++ Graph.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <tgmath.h>
 #include "GraphNode.h"
 #include "Edge.h"
 
@@ -45,6 +46,30 @@ class Graph{
 		//returns number of edges
 		int get_size(){
 			return edges.size();
+		}
+		
+		//calculates the distance between two nodes based on their coordinates
+		double get_distance(GraphNode n1, GraphNode n2){
+			int x1 = n1.get_x();
+			int y1 = n1.get_y();
+			int x2 = n2.get_x();
+			int y2 = n2.get_y();
+			
+			//take distance for x an y vals
+			int x_dist = x2 - x1;
+			int y_dist = y2 - y1;
+			
+			//square each distance
+			x_dist = x_dist * x_dist;
+			y_dist = y_dist * y_dist;
+			
+			//sum distances
+			double dist_sum = x_dist + y_dist;
+			
+			//sqrt the dist sum
+			double result = sqrt(dist_sum);
+			
+			return result;
 		}
 		
 		//takes a node ID and returns all nodes connected to that node
