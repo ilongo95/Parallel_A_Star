60e5f80b0769da2fe402be6138f1900cb74e5445
Nicholus Sakha <Nicholus.Sakha@gmail.com>
2020-12-09 22:09:52 -0500

    add runtime to output

--- test.cpp
+++ test.cpp
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sstream>
+#include <chrono>
 
 
 using namespace std;
@@ -35,7 +36,7 @@ Graph read_graph(char const* file_name) {
 	ifstream input_file(file_name);
 	string text;
 	std::vector<GraphNode> nodes;
-	std:vector<Edge> edges;
+std:vector<Edge> edges;
 	while (getline(input_file, text)) {
 		// Get tokens of line
 		istringstream ss(text);
@@ -50,7 +51,7 @@ Graph read_graph(char const* file_name) {
 		ss >> temp;
 		y = stoi(temp);
 		// Use these to create a graph node and store it in our vector
-		GraphNode *g = new GraphNode(name, x, y);
+		GraphNode* g = new GraphNode(name, x, y);
 		nodes.push_back(*g);
 	}
 	while (getline(input_file, text)) {
@@ -67,7 +68,7 @@ Graph read_graph(char const* file_name) {
 		v2 = nodes[index];
 		ss >> temp;
 		weight = stoi(temp);
-		Edge *e = new Edge(v1, v2, weight);
+		Edge* e = new Edge(v1, v2, weight);
 		edges.push_back(*e);
 	}
 	Graph g(nodes, edges);
@@ -86,11 +87,15 @@ int main(int argc, char** argv) {
 	int size_y = stoi(argv[3]);
 	int target = size_x * size_y - 1;
 
+
 	Graph g = read_graph(filename);
 
+	auto start = chrono::steady_clock::now();
 	std::vector<GraphNode> path = serial_a_star(g, 0, target);
+	auto end = chrono::steady_clock::now();
 
 	g.print_path(path);
+	cout << endl << "Runtime: " << double(chrono::duration_cast<chrono::microseconds>(end - start).count()) / 1000000 << " sec" << endl;
 
 	return 0;
 }
